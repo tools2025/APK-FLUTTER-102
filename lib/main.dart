9b72771010c0b0042186d2a13d772faeae991eb7
@tools2025 <xdtools9@gmail.com>
2026-01-08 16:53:05 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'screens/webview_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // ATUR STATUS BAR DI SINI - DIPAKSA
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent, // Transparan
+    statusBarIconBrightness: Brightness.light, // Ikon PUTIH
+    statusBarBrightness: Brightness.dark, // Mode gelap
+    systemNavigationBarColor: Color(0xFF0F0A2A), // Nav bar gelap
+    systemNavigationBarIconBrightness: Brightness.light, // Ikon nav PUTIH
+  ));
+  
+  // Set orientasi
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  
   runApp(const MyApp());
 }
 
@@ -14,6 +31,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      // PAKSA PAKAI THEME DARK
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color(0xFF0F0A2A),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF0F0A2A),
+          elevation: 0,
+          centerTitle: false,
+        ),
+      ),
       home: const WebViewScreen(),
     );
   }
