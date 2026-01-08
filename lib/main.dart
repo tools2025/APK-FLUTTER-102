413c6e884a6ffa9a56388862f8c806b7c358cab9
@tools2025 <xdtools9@gmail.com>
2026-01-08 17:15:07 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'screens/webview_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // **PAKSA ATUR DI AWAL SEBELUM APLIKASI DIMULAI**
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    statusBarIconBrightness: Brightness.light, // IKON PUTIH
-    statusBarBrightness: Brightness.dark,
-    systemNavigationBarColor: Color(0xFF0F0A2A),
-    systemNavigationBarIconBrightness: Brightness.light,
-  ));
-  
   runApp(const MyApp());
 }
 
@@ -25,9 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xFF0F0A2A),
-      ),
       home: const WebViewScreen(),
     );
   }
